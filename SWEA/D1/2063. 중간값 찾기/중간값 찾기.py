d4018dd8bf4eb5c7ded3688f4e0b2015552d6aa5
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2022-09-03 18:34:23 +0900

    [D1] Title: 중간값 찾기, Time: 147 ms, Memory: 58,544 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
@@ -1,15 +1,6 @@
-import java.util.Arrays;
-import java.util.Scanner;
+count = int(input())
+num =list(map(int, input().split()))
 
-public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[sc.nextInt()];
-		for(int i= 0;i<arr.length;i++) {
-			arr[i] = sc.nextInt();
-		}		
-		Arrays.sort(arr);
-		//N은 항상 홀수
-		System.out.println(arr[arr.length/2]);
-	}
-}
+num.sort()
+mid_num = (count//2)
+print(num[mid_num])

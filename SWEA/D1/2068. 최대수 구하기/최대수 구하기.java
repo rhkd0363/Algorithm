7eb23e8a3302f274bdede667dc0a541cfedf81bf
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2022-09-03 18:37:30 +0900

    [D1] Title: 최대수 구하기, Time: 129 ms, Memory: 20,300 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int T = sc.nextInt();
-		for (int i = 1; i <= T; i++) {
-			int max = 0;
-			for (int j = 0; j < 10; j++) {
-				int num = sc.nextInt();
-				if (max < num)
-					max = num;
+		int Tc = sc.nextInt();
+		int[] Data = {0,0,0,0,0,0,0,0,0,0};
+		for(int i = 1; i<Tc+1;i++) {
+			for (int j =0;j<10;j++) {
+				Data[j] = sc.nextInt();
 			}
-			System.out.println("#" + i + " " + max);
+			int MD = Data[0];
+			for(int K =1; K<10; K++) {
+				if(MD<Data[K])
+					MD = Data[K];
+			}
+			System.out.println("#"+i+" "+MD);
 		}
-	}
+		
+		}	
 }

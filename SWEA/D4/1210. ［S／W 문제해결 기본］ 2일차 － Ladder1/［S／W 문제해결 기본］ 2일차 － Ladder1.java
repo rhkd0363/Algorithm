0e927bdad0fdbe799bb8c70e6933aea0258f9e0e
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2022-09-03 18:21:11 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 152 ms, Memory: 30,228 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -1,44 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class  Solution {
-	public static void main(String[] args) throws IOException {
+public class Solution {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-
 		for (int t = 1; t <= 10; t++) {
 			int n = Integer.parseInt(br.readLine());
-			int[][] arr = new int[100][102];
-			List<Integer> iList = new ArrayList<>();
+			int[][] arr = new int[100][100];
 			int endPoint = 0;
-
 			for (int i = 0; i < 100; i++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 1; j < 101; j++) {
+				for (int j = 0; j < 100; j++) {
 					arr[i][j] = Integer.parseInt(st.nextToken());
 					if (arr[i][j] == 2)
 						endPoint = j;
-					else if (arr[i][j] == 1 && i == 0)
-						iList.add(j);
 				}
 			}
-
-			int x = 99, y = endPoint;
-
+			int[] dx = { -1, 0, 0 }, dy = { 0, -1, 1 };
+			int x = 99, y = endPoint, tx = 0, ty = 0;
 			while (x > 0) {
-				if (arr[x][y - 1] == 1) {
-					y = iList.get(iList.indexOf(y) - 1);
-				} else if (arr[x][y + 1] == 1) {
-					y = iList.get(iList.indexOf(y) + 1);
+				for (int d = 0; d < 3; d++) {
+					int nx = x + dx[d], ny = y + dy[d];
+					if (nx < 0 || ny < 0 || nx >= 100 || ny >= 100) {
+						continue;
+					}
+					else {
+						if (arr[nx][ny] == 1 && !(nx == tx && ny == ty)) {
+							tx = x;
+							ty = y;
+							x = nx;
+							y = ny;
+						}
+					}
 				}
-				x--;
 			}
-			
-			System.out.println("#" + t + " " + (y-1));
+			System.out.println("#" + t + " " + y);
 		}
 	}
 }

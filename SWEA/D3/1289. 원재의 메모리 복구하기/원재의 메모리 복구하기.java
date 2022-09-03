c474157bdf01a4ddb749fb5ff68e6008562de73a
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2022-09-03 18:18:53 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 131 ms, Memory: 20,220 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -1,21 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int T = sc.nextInt();
+		int T = Integer.parseInt(br.readLine());
 		
 		for(int t=1;t<=T;t++) {
-			String str = sc.next();
-			char[] ch = str.toCharArray();
+			String str = br.readLine();
 			
-			int[] arr = new int[ch.length];
+			int[] arr = new int[str.length()];
 			int cnt = 0;
 			for(int i =0;i<arr.length;i++) {
-				if(arr[i] != (ch[i]-'0')) {
+				if(arr[i] != (str.charAt(i)-'0')) {
 					for(int j=i;j<arr.length;j++) {
-						arr[j] = (ch[i]-'0');
+						arr[j] = (str.charAt(i)-'0');
 					}
 					cnt++;
 				}

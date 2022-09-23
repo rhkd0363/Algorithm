6827347148924d805308c8c8d66b39a2ec1fd3fc
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2022-09-23 19:52:30 +0900

    [Silver III] Title: N과 M (5), Time: 232 ms, Memory: 30148 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] nums;
+	static boolean[] check;
+	static int[] ans;
+	static StringBuilder SB = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		nums = new int[N];
+		check = new boolean[N];
+		ans = new int[M];
+
+		st = new StringTokenizer(br.readLine());
+		int i = 0;
+		while (st.hasMoreTokens()) {
+			nums[i++] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(nums);
+
+		NandM(0);
+
+		System.out.println(SB);
+	}
+
+	static void NandM(int idx) {
+		if (idx == M) {
+			for (int i = 0; i < M; i++) {
+				SB.append(ans[i]).append(' ');
+			}
+			SB.append('\n');
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (!check[i]) {
+				check[i] = true;
+				ans[idx] = nums[i];
+				NandM(idx + 1);
+				check[i] = false;
+			}
+		}
+	}
+}

fa5d2619a5bb301ae7fc12526854d058ca29ba72
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2022-10-05 02:41:10 +0900

    [D3] Title: [Professional] 조합, Time: 156 ms, Memory: 38,044 KB -BaekjoonHub

--- SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
+++ SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
@@ -1,49 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigDecimal;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+
+/*
+ * SWEA 5607 조합
+ * N comb R 의 값을 1234567891 로 나눈 나머지를 출력하는 문제
+ * 모듈러 연산
+ *  - 모듈러 연산은 나눗셈에서 사용할 수 없다.
+ *  - 나눗셈에서 사용이 불가하므로 페르마의 소정리를 이용해야 한다.
+ * 페르마의 소정리
+ * 	- P, Q가 서로소이다.
+ *  - P^(Q-1) mod Q 는 P의 항등원이다.
+ *  - P^(Q-2) mod Q 는 P의 역원이다.
+ * 조합
+ *  - N 에서 R 개를 뽑는 경우의 수 => N!/{(N-R)!*R!}
+ *  - 나눗셈에서 모듈러 연산이 불가능하므로 페르마 소정리 적용
+ *  - 위,아래변에  (N-R)!*R! 의 역원을 곱해준다.
+ *  - N! * (N-R! * R!)^(P-2)
+ */
 
 public class Solution {
-	static long P = 1234567891;
-	static long[] factorial;
+	static int mod = 1234567891;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		factorial = new long[1000001];
-		factorial[0] = 1;
-
+		// 팩토리얼의 모듈러 연산한 값을 배열에 저장한다.
+		long[] fact = new long[1000001];
+		fact[0] = 1;
 		for (int i = 1; i < 1000001; i++) {
-			factorial[i] = factorial[i - 1] * i;
-			factorial[i] %= P;
+			fact[i] = fact[i - 1] * i;
+			fact[i] %= mod;
 		}
 
-		int T = Integer.parseInt(br.readLine());
+		int T = sc.nextInt(); //테스트 케이스 입력
 
 		for (int t = 1; t <= T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = sc.nextInt();
+			int R = sc.nextInt();
 
-			int N = Integer.parseInt(st.nextToken());
-			int R = Integer.parseInt(st.nextToken());
-
-			long ans = factorial[N] * pow(factorial[N - R] * factorial[R] % P, P - 2)%P;
-
-			System.out.println("#"+t+" "+ans);
+			int result = (int) (fact[N] * pow(fact[N-R] * fact[R] % mod, mod-2) % mod);
+			
+			sb.append('#').append(t).append(' ').append(result).append('\n');
 		}
 
+		System.out.println(sb);
 	}
-
-	static long pow(long x, long n) {
-		if (n == 1)
-			return x;
-		long result = 0;
-		if (n % 2 == 0) {
-			result = pow(x, n / 2);
-			return result * result % P;
-		}
-		result = pow(x, n - 1) % P;
-		return (result * x) % P;
-
+	
+	static long pow(long a, long b) {
+		// 분할 정복 사용. ex) 2^6 = 2^3 * 2^3 = ...
+		if(b == 1)
+			return a;
+		
+		long result = pow(a,b/2);
+		
+		if(b%2 == 0) 
+			return (result * result) % mod;
+		else
+			return (((result * result) % mod) * a) % mod;
 	}
 }

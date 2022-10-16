2c012b6f6e7a6aa29b5c00808955e1c80209db5e
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2022-10-16 21:20:40 +0900

    [Silver II] Title: 최대 힙, Time: 300 ms, Memory: 25792 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				return o2-o1;
+			}
+		});
+		
+		for(int i=0;i<N;i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(num == 0) {
+				if(pq.size() == 0)
+					sb.append(0).append('\n');
+				else
+					sb.append(pq.poll()).append('\n');
+				continue;
+			}
+			pq.add(num);
+		}
+		
+		System.out.println(sb);
+	}
+}

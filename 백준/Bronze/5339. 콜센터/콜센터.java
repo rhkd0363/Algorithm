254dcff7eb0ad69c89526ee0f4b1b758f1cfe240
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2022-11-09 22:20:07 +0900

    [Bronze V] Title: 콜센터, Time: 68 ms, Memory: 11360 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("     /~\\\n" + "    ( oo|\n" + "    _\\=/_\n" + "   /  _  \\\n" + "  //|/.\\|\\\\\n"
+				+ " ||  \\ /  ||\n" + "============\n" + "|          |\n" + "|          |\n" + "|          |");
+	}
+}

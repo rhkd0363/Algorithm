aed76764a9d9a3dfe2db557d00454804050d0b91
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2023-02-04 23:53:46 +0900

    [Gold III] Title: 역사, Time: 1280 ms, Memory: 43436 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.java
+++ 백준/Gold/1613. 역사/역사.java
@@ -1,41 +1,48 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int n = sc.nextInt();
-    int k = sc.nextInt();
-    int[][] arr = new int[n + 1][n + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[n + 1][n + 1];
 
-    for (int i = 0; i < k; i++) {
-      int x = sc.nextInt();
-      int y = sc.nextInt();
-      arr[x][y] = -1;
-      arr[y][x] = 1;
-    }
+		for (int i = 0; i < k; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        for (int l = 1; l <= n; l++) {
-          if (arr[j][i] == -1 && arr[i][l] == -1) {
-            arr[j][l] = -1;
-            arr[l][j] = 1;
-          }
-        }
-      }
-    }
+			arr[x][y] = -1; // 전
+			arr[y][x] = 1; // 후
+		}
 
-    int s = sc.nextInt();
-    for (int i = 0; i < s; i++) {
-      int x = sc.nextInt();
-      int y = sc.nextInt();
-      
-      if(x < 1 || y < 1 || x > n || y > n) {
-    	  System.out.println(0);
-      }else {
-    	  System.out.println(arr[x][y]);
-      }
-    }
-  }
+		for (int i = 1; i <= n; i++) { // 경
+			for (int j = 1; j <= n; j++) { // 출
+				for (int l = 1; l <= n; l++) { // 도
+					if (arr[j][i] == -1 && arr[i][l] == -1) {
+						arr[j][l] = -1; // 전
+						arr[l][j] = 1; // 후
+					}
+				}
+			}
+		}
+
+		int s = Integer.parseInt(br.readLine());
+		for (int i = 0; i < s; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			if (x < 1 || y < 1 || x > n || y > n) { // 범위 벗어나는거
+				System.out.println(0);
+			} else {
+				System.out.println(arr[x][y]);
+			}
+		}
+	}
 }

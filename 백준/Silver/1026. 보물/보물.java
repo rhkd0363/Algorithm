a65d3147e07b03f71f4dec9006fe000bdb827d31
rhkd0363 <62332870+rhkd0363@users.noreply.github.com>
2023-02-05 20:50:11 +0900

    [Silver IV] Title: 보물, Time: 120 ms, Memory: 12992 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    int[] b = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = sc.nextInt();
+    }
+    for (int i = 0; i < n; i++) {
+      b[i] = sc.nextInt();
+    }
+    Arrays.sort(a);
+    Arrays.sort(b);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += a[i] * b[n - i - 1];
+    }
+    System.out.println(sum);
+  }
+}

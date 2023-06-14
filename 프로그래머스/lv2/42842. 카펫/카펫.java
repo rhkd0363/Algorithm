345150a7a5ca3ae16b292272f9c08800e21bb304
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-14 17:13:48 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+         for(int column=1;column<=Math.sqrt(yellow);column++){
+             if(yellow % column == 0){
+                int row = yellow / column;
+                 
+                 if(row * 2 + column * 2 + 4 == brown){
+                     answer =  new int[]{row+2,column+2};
+                 }
+             }
+         }
+        return answer;
+                 
+    }
+}

6abd32f42a4052ae102c6618640b0e56cab00fcb
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-15 17:32:00 +0900

    [Gold IV] Title: N-Queen, Time: 4584 ms, Memory: 12132 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,60 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[][] map;
-	static int N;
-	static int res;
-//	static int[] res;
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int N, result;
+	static int[] choice;
+
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-//		res = new int[N];
-		res = 0;
+		result = 0;
 
-		putQueen(0);
-		
-		System.out.println(res);
+		choice = new int[N + 1];
+
+		DFS(1);
+
+		System.out.println(result);
 	}
 
-	static void putQueen(int r) {
-		if(r == N) {
-//			StringBuilder sb = new StringBuilder();
-//			for(int i=0;i<N;i++) {
-//				sb.append(res[i]).append('\n');
-//			}
-//			System.out.print(sb);
-//			System.exit(0);
-			res++;
+	static void DFS(int line) {
+		if (line == N + 1) {
+			result++;
 			return;
 		}
-		
-		for(int c=0;c<N;c++) {
-			if(checkQueen(r,c)) {
-				map[r][c] = 1;
-//				res[r] = c+1;
-				putQueen(r+1);
-				map[r][c] = 0;
-//				res[r] = 0;
-			}
-		}
-	}
 
-	static boolean checkQueen(int r, int c) {
-		int left = c - 1;
-		int right = c + 1;
-		
-		for (int i = r - 1; i >= 0; i--) {
-			if (map[i][c] == 1 || (right < N && map[i][right++] == 1)
-					|| (left >= 0 &&map[i][left--] == 1))
-				return false;
+		loop: for (int i = 1; i <= N; i++) {
+			for (int j = 1; j < line; j++) {
+				if (choice[j] == i || choice[j] + line - j == i || choice[j] - (line - j) == i) {
+					continue loop;
+				}
+			}
+			choice[line] = i;
+
+			DFS(line + 1);
 		}
-		
-		return true;
+
 	}
 }

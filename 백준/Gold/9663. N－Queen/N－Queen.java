4886e36a12d2d3100f644d3a62be874a78c0a2ec
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-15 17:35:35 +0900

    [Gold IV] Title: N-Queen, Time: 4568 ms, Memory: 12136 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,17 +4,12 @@ import java.util.*;
 public class Main {
 	static int N, result;
 	static int[] choice;
-
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		N = Integer.parseInt(br.readLine());
 		result = 0;
-
 		choice = new int[N + 1];
-
 		DFS(1);
-
 		System.out.println(result);
 	}
 
@@ -23,7 +18,6 @@ public class Main {
 			result++;
 			return;
 		}
-
 		loop: for (int i = 1; i <= N; i++) {
 			for (int j = 1; j < line; j++) {
 				if (choice[j] == i || choice[j] + line - j == i || choice[j] - (line - j) == i) {
@@ -31,9 +25,7 @@ public class Main {
 				}
 			}
 			choice[line] = i;
-
 			DFS(line + 1);
 		}
-
 	}
 }

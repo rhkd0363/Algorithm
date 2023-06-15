644b489b6fdd60197444c25bf12597a4f82c9c63
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-15 19:41:24 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 9.10 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    static boolean[][] map;
+    static int answer,N;
+    
+    public int solution(int n, int[][] wires) {
+        N = n;
+        map = new boolean[N][N];
+        
+        for(int i=0;i<wires.length;i++){
+            map[wires[i][0]-1][wires[i][1]-1] = true;
+            map[wires[i][1]-1][wires[i][0]-1] = true;
+        }
+            
+        answer = Integer.MAX_VALUE;
+        
+        DFS(0,0,0);
+        
+        return answer;
+    }
+    
+    
+    static int DFS(int idx, int cnt, int from){
+        for(int i=0;i<N;i++){
+            if(map[idx][i] && i != from){
+                
+                cnt += DFS(i,1, idx);
+            
+            }
+        }
+        
+        answer = Math.min(answer, Math.abs(Math.abs(N-cnt) - cnt));
+        
+        System.out.println(idx+1+" "+cnt);
+        
+        return cnt;
+    }
+    
+}

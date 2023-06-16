f396390e78dae8e1a1b1f6f8e16264ff1999babc
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-16 11:43:25 +0900

    [Silver II] Title: 늑대와 올바른 단어, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/13022. 늑대와 올바른 단어/늑대와 올바른 단어.java
+++ 백준/Silver/13022. 늑대와 올바른 단어/늑대와 올바른 단어.java
@@ -1,24 +1,17 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	static int N, result;
-	static int[] choice;
-
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String input = br.readLine();
-		
+		String input = br.readLine();		
 		char prevChar = 'f';
 		boolean flag = true;
-		
 		int prevLength = 1;
 		int currLength = 1;
 		
 		loop : for(int i=0;i<input.length();i++) {
 			char currChar = input.charAt(i);
-
 			if(input.charAt(i) != prevChar) {
 				switch (currChar) {
 					case 'w':{
@@ -70,14 +63,11 @@ public class Main {
 						break loop;
 					}
 				}
-			}
-			
+			}		
 			currLength++;
-		}
-		
+		}	
 		if(prevChar != 'f' || prevLength != currLength)
-			flag = false;
-		
+			flag = false;	
 		System.out.println(flag ? 1 : 0);
 	}
 }

2b456f2ace578607be8025087a3cbf32025efd69
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-21 11:44:27 +0900

    [level 2] Title: 스킬트리, Time: 7.49 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -6,16 +6,12 @@ class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
+        String pattern = "[^"+skill+"]";
+        
         for(String skill_tree : skill_trees){
-            String[] temp = skill_tree.split("");
-            for(int i=0;i<temp.length;i++){
-                if(!skill.contains(temp[i])){
-                    skill_tree = skill_tree.replace(temp[i],"");
-                }
-            }
+            skill_tree = skill_tree.replaceAll(pattern,"");
             
-            if(skill.indexOf(skill_tree) == 0)
-                answer++;
+            answer += skill.indexOf(skill_tree) == 0 ? 1 : 0;
         }
         
         return answer;

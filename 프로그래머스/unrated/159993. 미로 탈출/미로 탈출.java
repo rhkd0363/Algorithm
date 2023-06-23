c0d9cd8e526b8f29cba6f056e854c0bddc65c2ca
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-23 19:06:29 +0900

    [unrated] Title: 미로 탈출, Time: 0.39 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.java
@@ -5,84 +5,53 @@ class Solution {
         int x;
         int y;
         int count;
-        
         public Info(int x, int y, int count){
             this.x = x;
             this.y = y;
             this.count = count;
         }
     }
-    
-    public int solution(String[] maps) {
-        
-        boolean[][] visited_B = new boolean[maps.length][maps[0].length()];
-        boolean[][] visited_A = new boolean[maps.length][maps[0].length()];
-        
-        Queue<Info> queue_B = new LinkedList<>();
-        Queue<Info> queue_A = new LinkedList<>();
-        
+    static String[] map;
+    static boolean flag;
+    static Info pos;
+    public int solution(String[] maps) {        
+        map = maps; flag = false;
         loop : for(int i=0;i<maps.length;i++)
             for(int j=0;j<maps[i].length();j++)
                 if(maps[i].charAt(j) == 'S'){
-                    queue_B.add(new Info(i, j, 0));
+                    pos = BFS(new Info(i, j, 0),maps.length, maps[0].length(), 'L');
                     break loop;
                 }
-        
-        boolean flag = false;
-        
-        int[][] dxdy = {{-1,0},{0,-1},{0,1},{1,0}};
-
+        if(flag == false)
+            return -1;
+        flag = false;
         int answer = 0;
-        
-        while(!queue_B.isEmpty()){
-            Info curr = queue_B.poll();
-            
-            if(maps[curr.x].charAt(curr.y) == 'L'){
-                queue_A.add(new Info(curr.x, curr.y, curr.count));
-                break;
-            }
-            
-            if(visited_B[curr.x][curr.y])
-                continue;
-            
-            visited_B[curr.x][curr.y] = true;
-            
-            for(int i=0;i<4;i++){
-                int nx = curr.x + dxdy[i][0];
-                int ny = curr.y + dxdy[i][1];
-                
-                if(nx < 0 || ny < 0 || nx >= visited_B.length || ny >= visited_B[0].length || maps[nx].charAt(ny) == 'X')
-                    continue;
-                
-                queue_B.add(new Info(nx, ny, curr.count+1));
-            }
-        }
-        
-        while(!queue_A.isEmpty()){
-            Info curr = queue_A.poll();
-            
-            if(maps[curr.x].charAt(curr.y) == 'E'){
-                flag = true;
-                answer = curr.count;
-                break;
-            }
-            
-            if(visited_A[curr.x][curr.y])
-                continue;
-            
-            visited_A[curr.x][curr.y] = true;
-            
-            for(int i=0;i<4;i++){
-                int nx = curr.x + dxdy[i][0];
-                int ny = curr.y + dxdy[i][1];
-                
-                if(nx < 0 || ny < 0 || nx >= visited_A.length || ny >= visited_A[0].length || maps[nx].charAt(ny) == 'X')
-                    continue;
-                
-                queue_A.add(new Info(nx, ny, curr.count+1));
-            }
-        }
-        
+        pos = BFS(pos, maps.length, maps[0].length(), 'E');
+        answer = pos.count;
         return flag ? answer : -1;
+    }
+    static Info BFS(Info info, int rowLen, int colLen, char target){
+        Queue<Info> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[rowLen][colLen];
+        int[][] dxdy = {{-1,0},{0,-1},{0,1},{1,0}};
+        queue.add(info);
+        while(!queue.isEmpty()){
+            Info curr = queue.poll();
+            if(map[curr.x].charAt(curr.y) == target){
+                flag = true;
+                return curr;
+            }
+            if(visited[curr.x][curr.y])
+                continue;
+            visited[curr.x][curr.y] = true;
+            for(int i=0;i<4;i++){
+                int nx = curr.x + dxdy[i][0];
+                int ny = curr.y + dxdy[i][1];
+                if(nx < 0 || ny < 0 || nx >= rowLen || ny >= colLen || map[nx].charAt(ny) == 'X')
+                    continue;
+                queue.add(new Info(nx, ny, curr.count+1));
+            }
+        }
+        return new Info(0,0,-1);
     }
 }

6e8736519477e14dfd865a10d4cdb1e1c2a02287
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-27 16:22:29 +0900

    [Gold V] Title: AC, Time: 540 ms, Memory: 79664 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,73 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
+		
 		int T = Integer.parseInt(br.readLine());
-
-		testcase: for (int t = 0; t < T; t++) {
-			String command = br.readLine();
-			int length = Integer.parseInt(br.readLine());
-
-			List<Integer> list = new ArrayList<>();
-
-			String arr = br.readLine();
-
-			if (length != 0) {
-				int idx = 1;
-				// 맨앞뒤 괄호를 빼고진행
-				for (int i = 1; i < arr.length() - 1; i++) {
-					if (arr.charAt(i) == ',') {
-						list.add(Integer.parseInt(arr.substring(idx, i)));
-						idx = i + 1;
-					}
-				}
-				list.add(Integer.parseInt(arr.substring(idx, arr.length() - 1)));
+		
+		for(int tc=0;tc<T;tc++) {
+			String functions = br.readLine();
+			
+			int n = Integer.parseInt(br.readLine());
+			
+			String inputLine = br.readLine(); 
+			
+			String[] list = inputLine.substring(1, inputLine.length() - 1).split(",");
+			
+			boolean flag = true; // true : 정방향 ,false : 역방향
+			
+			int front = 0;
+			int rear = 0;
+			
+			for(int i=0;i<functions.length();i++) {
+				flag = functions.charAt(i) == 'R' ? !flag : flag;
+				front += functions.charAt(i) == 'D' && flag ? 1 : 0;
+				rear += functions.charAt(i) == 'D' && !flag ? 1 : 0;
 			}
-
-			boolean check = true;
-
-			for (int i = 0; i < command.length(); i++) {
-				if (command.charAt(i) == 'R')
-					check = !check;
-				if (command.charAt(i) == 'D') {
-					if (list.size() == 0) {
-						sb.append("error").append('\n');
-						continue testcase;
-					}
-					if (check)
-						list.remove(0);
-					else
-						list.remove(list.size() - 1);
-				}
+			
+			if(front+rear > n) {
+				sb.append("error").append('\n');
+				continue;
 			}
-
-			sb.append('[');
-			if (check) {
-				for (int i = 0; i < list.size()-1; i++) {
-					sb.append(list.get(i));
-					sb.append(',');
+			
+			sb.append("[");
+			
+			if(flag) {
+				for(int i=front;i< list.length - rear;i++) {
+					sb.append(list[i]).append(',');
 				}
-				if (list.size() != 0)
-					sb.append(list.get(list.size()-1));
-			} else {
-				for (int i = list.size() - 1; i > 0; i--) {
-					sb.append(list.get(i));
-					sb.append(',');
-				}
-				if (list.size() != 0)
-					sb.append(list.get(0));
+			}else {
+				for(int i=list.length - rear -1; i >= front; i--) {
+					sb.append(list[i]).append(',');
+				}				
 			}
+			
+			if(sb.charAt(sb.length()-1) == ',')
+				sb.deleteCharAt(sb.length() - 1);
+			
 			sb.append(']').append('\n');
 		}
+		
 		System.out.println(sb);
 	}
 }

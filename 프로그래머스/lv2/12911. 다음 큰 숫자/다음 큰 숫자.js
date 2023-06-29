91ad1ff4d7c01af764df85b894f767bef67503fb
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-29 17:48:24 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,8 +1,8 @@
 function solution(n) {
-    let binaryN = n.toString(2).replaceAll('0','')
+    let binaryN = n.toString(2).match(/1/g).length
     
     while(true){
-        if(binaryN === (++n).toString(2).replaceAll('0','')){
+        if(binaryN === (++n).toString(2).match(/1/g).length){
             return n;
         }
     }

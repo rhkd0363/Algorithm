9d84e97a3d6cc73b00a069eae8572f1770047fa1
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-29 18:26:24 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,11 +1,1 @@
-function solution(n)
-{
-    let answer = 0;
-    
-    while(n !== 0){
-        answer += n % 2 == 1 ? 1 : 0
-        n = Math.floor(n/2)
-    }
-    
-    return answer;
-}
+const solution = (n) => n.toString(2).match(/1/g).length

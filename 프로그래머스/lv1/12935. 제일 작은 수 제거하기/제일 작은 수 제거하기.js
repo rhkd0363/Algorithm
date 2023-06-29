cf3526a37ac7bc24a20b5bb67e8917e61f56fcbf
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-06-29 17:25:42 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,4 +1,4 @@
 function solution(arr) {
-    arr.splice(arr.reduce((acc, curr, idx) => acc[0] > curr ? acc = [curr, idx] : acc, [arr[0], 0])[1],1);
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
     return arr.length !== 0 ? arr : [-1];
 }

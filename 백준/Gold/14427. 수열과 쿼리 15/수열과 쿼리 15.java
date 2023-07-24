1c7525404b17cbfa791caa441a43a1c874b7cadd
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-07-24 17:22:43 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 552 ms, Memory: 54072 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
@@ -2,54 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Item implements Comparable<Item> {
-		int index;
-		int value;
-
-		public Item(int index, int value) {
-			this.index = index;
-			this.value = value;
-		}
-
-		public int compareTo(Item o) {
-			if (this.value == o.value)
-				return this.index - o.index;
-			return this.value - o.value;
-		}
-	}
+	static int[] segmentTree, value;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
 		int N = Integer.parseInt(br.readLine());
-
-		Item[] items = new Item[N + 1];
-
+		segmentTree = new int[N * 4];
+		value = new int[N + 1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		TreeSet<Item> set = new TreeSet<>();
-
+        
 		for (int i = 1; i <= N; i++) {
-			items[i] = new Item(i, Integer.parseInt(st.nextToken()));
-			set.add(items[i]);
+			value[i] = Integer.parseInt(st.nextToken());
 		}
-
+		setTree(1, 1, N);
 		int M = Integer.parseInt(br.readLine());
-
+        
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-
-			if (Integer.parseInt(st.nextToken()) == 2) {
-				sb.append(set.first().index).append('\n');
+			if (Integer.parseInt(st.nextToken()) == 1) {
+				int index = Integer.parseInt(st.nextToken());
+				value[index] = Integer.parseInt(st.nextToken());
+				updateTree(1, index, 1, N);
 			} else {
-				int idx = Integer.parseInt(st.nextToken());
-				int value = Integer.parseInt(st.nextToken());
-				set.remove(items[idx]);
-				items[idx].value = value;
-				set.add(items[idx]);
+				sb.append(segmentTree[1]).append('\n');
 			}
 		}
-
 		System.out.println(sb);
+	}
+
+	static int setTree(int node, int start, int end) {
+		if (start == end) {
+			return segmentTree[node] = start;
+		}
+		int mid = (start + end) / 2;
+		int left = setTree(node * 2, start, mid);
+		int right = setTree(node * 2 + 1, mid + 1, end);
+		if (value[left] <= value[right]) {
+			return segmentTree[node] = left;
+		}
+		return segmentTree[node] = right;
+	}
+
+	static int updateTree(int node, int idx, int start, int end) {
+		if (start == end) {
+			return start;
+		}
+		if (start <= idx && idx <= end) {
+			int mid = (start + end) / 2;
+			int left = updateTree(node * 2, idx, start, mid);
+			int right = updateTree(node * 2 + 1, idx, mid + 1, end);
+			if (value[left] <= value[right]) {
+				return segmentTree[node] = left;
+			}
+			return segmentTree[node] = right;
+		}
+		return segmentTree[node];
 	}
 }

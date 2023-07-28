f1194ef4b626951ce51b1aa594ae7081edb2c54e
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-07-28 19:35:22 +0900

    [Silver IV] Title: 도미노 무너트리기, Time: 1096 ms, Memory: 158156 KB -BaekjoonHub

--- 백준/Silver/25972. 도미노 무너트리기/도미노 무너트리기.java
+++ 백준/Silver/25972. 도미노 무너트리기/도미노 무너트리기.java
@@ -1,50 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException,IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-
-        Node[] arr = new Node[n];
-
-        for(int i = 0; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+        List<int[]> list = new ArrayList<>();
+        for(int i=0;i<N;i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()), l = Integer.parseInt(st.nextToken());
-
-            arr[i] = new Node(a, l);
+            list.add(new int[]{Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())});
         }
-
-        Arrays.sort(arr);
-
-        int answer = 0, which = -1;
-        for(int i = 0; i < n; i++) {
-            if(arr[i].a > which) {
-                answer++;
+        Collections.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
             }
-            which = arr[i].a + arr[i].l;
+        });
+        int max = 0;
+        int answer = 0;
+        for (int[] curr : list){
+            answer += max < curr[0] ? 1 : 0;
+            max = curr[0] + curr[1];
         }
-        System.out.print(answer);
-    }
-}
-
-class Node implements Comparable<Node>{
-    int a, l;
-
-    public Node(int a, int l) {
-        this.a = a;
-        this.l = l;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        if(this.a == o.a) {
-            return o.l - this.l;
-        }
-        return this.a - o.a;
+        System.out.println(answer);
     }
 }

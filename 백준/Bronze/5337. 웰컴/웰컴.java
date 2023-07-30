caeb4d4d415fde8646be8d041bf8eeec3077505b
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-07-30 16:49:07 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11296 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+
+    }
+}

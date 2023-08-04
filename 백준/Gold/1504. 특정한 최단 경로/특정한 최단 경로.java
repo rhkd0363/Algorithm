38816f4b730a532506337272e065a3e62f1d1c71
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-08-04 22:33:06 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 564 ms, Memory: 68532 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,144 +1,87 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	static int N, E, V1, V2;
-	static List<Node>[] adjList;
-	static boolean[] visited;
-	static double[] dist;
-	
-	static class Node implements Comparable<Node>{
-		int v;
-		double weight;
-		
-		public Node(int v, double weight) {
-			super();
-			this.v = v;
-			this.weight = weight;
-		}
+public class Main {
+    static class Node implements Comparable<Node> {
+        int end, cost;
 
-		@Override
-		public int compareTo(Node o) {
-			return Double.compare(this.weight, o.weight);
-		}
-	}
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		// 정점의 수, 간선의 수 입력
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		
-		// 인접리스트 생성 및 초기화
-		adjList = new ArrayList[N+1];
-		for(int i=0;i<N+1;i++) {
-			adjList[i] = new ArrayList<>();
-		}
-		
-		
-		// 간선 정보 입력
-		for(int i=0;i<E;i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			
-			adjList[start].add(new Node(end, weight));
-			adjList[end].add(new Node(start, weight));
-		}
-		
-		// 지나야 하는 두개의 정점 입력
-		st = new StringTokenizer(br.readLine());
-		V1 = Integer.parseInt(st.nextToken());
-		V2 = Integer.parseInt(st.nextToken());
-		
-		//정답 가능한 상황
-		//1 -> v1 -> v2 -> N
-		//1 -> v2 -> v1 -> N
-		//1 -> v1 -> 1 -> v2 -> N
-		//1 -> v2 -> 1 -> v1 -> N
-		//1 -> v1 -> 1 -> v2 -> 1 -> N
-		
-		double[] ans = new double[5];
-		
-		// 거리 배열 생성 및 초기화
-		dist = new double[N+1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		
-		// 방문 배열 생성
-		visited = new boolean[N+1];
-				
-		
-		dijkstra(1);
-		ans[0] = dist[V1];
-		ans[1] = dist[V2];
-		ans[2] = dist[V1]*2 + dist[V2];
-		ans[3] = dist[V2]*2 + dist[V1];
-		ans[4] = dist[V2]*2 + dist[V1]*2 + dist[N]; //완료
-		
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		visited = new boolean[N+1];
-		
-		dijkstra(V1);
-		
-		ans[0]   += dist[V2];
-		ans[1]   += dist[N];
-		ans[3] += dist[N]; // 완료
-		
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		visited = new boolean[N+1];
-		
-		dijkstra(V2);
-		
-		ans[0]   += dist[N]; // 완료
-		ans[1]   += dist[V1]; //완료
-		ans[2] += dist[N]; //완료
-		
-		double res = Integer.MAX_VALUE;
-		
-		for(int i=0;i<5;i++) {
-			res = Math.min(res, ans[i]);
-		}
-		
-		
-		if(Math.abs(res) >= Integer.MAX_VALUE)
-			System.out.println(-1);
-		else
-			System.out.println((int)res);
-		
-	}
-	
-	// 1번 정점에서 V1 가는 거리, 1번 정점에서 V2 가는 거리, V1에서 V2 가는 거리, V1.V2 에서 N 가는 거리
+        public Node(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
 
-	static void dijkstra(int str) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(str,0));
-		dist[str] = 0;
-		
-		while(!pq.isEmpty()) {
-			Node curr = pq.poll();
-			
-			if(visited[curr.v])
-				continue;
-			
-			visited[curr.v] = true;
-			
-			for(Node node : adjList[curr.v]) {
-				if(!visited[node.v] && dist[node.v] > dist[curr.v] + node.weight) {
-					dist[node.v] = dist[curr.v] + node.weight;
-					pq.add(new Node(node.v, dist[node.v]));
-				}
-			}
-		}
-	}
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+
+        List<Node>[] list = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++)
+            list[i] = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            list[start].add(new Node(end, cost));
+            list[end].add(new Node(start, cost));
+        }
+        st = new StringTokenizer(br.readLine());
+        int firstPoint = Integer.parseInt(st.nextToken());
+        int secondPoint = Integer.parseInt(st.nextToken());
+
+        int startToFirst = dijkstra(1, firstPoint, N, list);
+        int firstToSecond = dijkstra(firstPoint, secondPoint, N, list);
+        int secondToEnd = dijkstra(secondPoint, N, N, list);
+
+        int fSum = startToFirst+firstToSecond+secondToEnd;
+
+        int startToSecond = dijkstra(1, secondPoint, N, list);
+        int secondToFirst = dijkstra(secondPoint, firstPoint, N, list);
+        int firstToEnd = dijkstra(firstPoint, N, N, list);
+
+        int sSum = startToSecond+secondToFirst+firstToEnd;
+
+        System.out.println(Math.min(fSum, sSum));
+    }
+
+    static int dijkstra(int start, int end, int N, List<Node>[] list) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        int[] costs = new int[N + 1];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        boolean[] visited = new boolean[N + 1];
+        pq.add(new Node(start,0));
+        costs[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node curr = pq.poll();
+            if(curr.end == end)
+                break;
+            if(visited[curr.end])
+                continue;
+            visited[curr.end] = true;
+            for (Node node : list[curr.end]) {
+                if (!visited[node.end] && costs[node.end] > costs[curr.end] + node.cost) {
+                    costs[node.end] = costs[curr.end] + node.cost;
+                    pq.add(new Node(node.end, costs[node.end]));
+                }
+            }
+        }
+        if(costs[end] == Integer.MAX_VALUE){
+            exit();
+        }
+        return  costs[end];
+    }
+
+    static void exit(){
+        System.out.println(-1);
+        System.exit(0);
+    }
 }

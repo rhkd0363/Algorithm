55078d1ab0c60dea888a12a0767d542112208deb
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-08-22 21:30:45 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 116 ms, Memory: 12696 KB -BaekjoonHub

---
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[10001][4];
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for(int i=4;i<10001;i++){
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][1] + dp[i-2][2];
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+        }
+
+        for(int t=0;t<T;t++){
+            int input = Integer.parseInt(br.readLine());
+            System.out.println(dp[input][1]+dp[input][2]+dp[input][3]);
+        }
+    }
+}

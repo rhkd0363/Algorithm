57544f56de85b65344d7f56fb0a9484aa43d1d0b
신현광 <62332870+rhkd0363@users.noreply.github.com>
2023-10-13 23:56:28 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 68 ms, Memory: 11300 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+
+    }
+}
